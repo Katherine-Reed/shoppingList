ab3829791a7567fa163c59257acc6cfc593d343c
Kat Grace <katherine.grace.reed@gmail.com>
2024-05-07 20:51:02 +0100

    Final updates (maybe)

--- index.js
+++ index.js
@@ -22,17 +22,21 @@ addButtonEl.addEventListener("click", function() {
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    let itemsArray = Object.entries(snapshot.val())
-    
-    clearShoppingListEl()
-    
-    for (let i = 0; i < itemsArray.length; i++) {
-        let currentItem = itemsArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        
-        appendItemToShoppingListEl(currentItem)
-    }
+    if (snapshot.exists()) {	
+		let itemsArray = Object.entries(snapshot.val())
+		
+		clearShoppingListEl()
+		
+		for (let i = 0; i < itemsArray.length; i++) {
+			let currentItem = itemsArray[i]
+			let currentItemID = currentItem[0]
+			let currentItemValue = currentItem[1]
+			
+			appendItemToShoppingListEl(currentItem)
+		}
+    } else {
+		shoppingListEl.innerHTML = "Nothing here... yet!"
+	}
 })
 
 function clearShoppingListEl() {
